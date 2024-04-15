10bd3e6f0c0ecc6325248f1ac8d1158de07c0d8b
Sohaib Raja <raja.sohaib7743@gmail.com>
2024-04-15 16:44:22 -0500

    converted Distance estimation to v8

---
+++ FocalCalculation.py
@@ -0,0 +1,106 @@
+import cv2 as cv 
+import numpy as np
+import scipy.interpolate as spi
+
+# Distance constants 
+REFERENCE_DISTANCES = [52.476, 101.22, 138.588, 177.756, 227.796, 267.396, 325.152, 390, 465.084, 532.836, 621.54, 966.264, 1291.2, 1484.76]
+CAR_REF_WIDTH = 78.6 #INCHES
+AVG_CAR_WIDTH = 70 #INCHES
+KNOWN_DISTANCE = 45 #INCHES
+PERSON_WIDTH = 16 #INCHES
+
+# Object detector constant 
+CONFIDENCE_THRESHOLD = 0.4
+NMS_THRESHOLD = 0.3
+ 
+# colors for object detected
+COLORS = [(255,0,0),(255,0,255),(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
+GREEN = (0,255,0)
+LIGHT_RED = (75, 71, 255)
+CYAN = (0, 255, 255)
+WHITE = (255,255,255)
+BLACK =(0,0,0)
+# defining fonts 
+FONTS = cv.FONT_HERSHEY_COMPLEX
+
+def CalculateFocalLengths():
+
+    # getting class names from classes.txt file 
+    class_names = []
+    with open("classes.txt", "r") as f:
+        class_names = [cname.strip() for cname in f.readlines()]
+    #  setttng up opencv net
+    yoloNet = cv.dnn.readNet('yolov4-tiny.weights', 'yolov4-tiny.cfg')
+
+    yoloNet.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
+    yoloNet.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA_FP16)
+
+    model = cv.dnn_DetectionModel(yoloNet)
+    model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
+
+    # object detector funciton /method
+    def object_detector(image):
+        classes, scores, boxes = model.detect(image, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
+        # creating empty list to add objects data
+        data_list =[]
+        for (classid, score, box) in zip(classes, scores, boxes):
+            # define color of each, object based on its class id 
+            color= COLORS[int(classid) % len(COLORS)]
+        
+            label = "%s : %f" % (class_names[classid], score)
+
+            # draw rectangle on and label on object
+            cv.rectangle(image, box, color, 2)
+            cv.putText(image, label, (box[0], box[1]-14), FONTS, 0.5, color, 2)
+        
+            # getting the data 
+            # 1: class name  2: object width in pixels, 3: position where have to draw text(distance)
+            if classid ==0: # person class id 
+                data_list.append([class_names[classid], box[2], (box[0], box[1]-2), box[3]])
+            elif classid ==2: #car class id
+                data_list.append([class_names[classid], box[2], (box[0], box[1]-2), box[3]])
+            elif classid == 7: #truck class id
+                data_list.append([class_names[classid], box[2], (box[0], box[1]-2), box[3]])
+            # if you want inclulde more classes then you have to simply add more [elif] statements here
+            # returning list containing the object data. 
+        return data_list
+
+    def focal_length_finder (measured_distance, real_width, width_in_rf):
+        focal_length = (width_in_rf * measured_distance) / real_width
+
+        return focal_length
+
+    # reading the reference image from dir 
+    ref_person = cv.imread('ReferenceImages/person.jpeg')
+    person_data = object_detector(ref_person)
+    person_width_in_rf = person_data[0][1]
+
+    car_focal_lengths = []
+
+    # throw in closest image that is not recognized
+    car_widths = [1106]
+    car_focal_lengths.append(focal_length_finder(25.98, CAR_REF_WIDTH, 1106))
+
+    for i in range(len(REFERENCE_DISTANCES)):
+        ref_car = cv.imread('ReferenceImages/car' + str(i) + '.png')
+        car_data = object_detector(ref_car)
+
+        # ignore objects in the background
+        car_width = car_data[0][1]
+        if len(car_data) > 1:
+            car_width = max(item[1] for item in car_data)
+        car_widths.append(car_width)
+
+        print(f"Car {i} width in pixels: {car_width} - Distance: {REFERENCE_DISTANCES[i]} inches")
+        car_focal_lengths.append(focal_length_finder(REFERENCE_DISTANCES[i], CAR_REF_WIDTH, car_width))
+
+    # add filler values for when at 0 pixels and 1440 pixels
+    # assume average focal length at far distances and 500 at close distances
+    car_widths.append(0)
+    car_focal_lengths.append(1440)
+    car_widths.append(1440)
+    car_focal_lengths.append(200)
+
+    # interpolate the focal lengths
+    interpolate_focal = spi.interp1d(car_widths, car_focal_lengths, kind='cubic')
+    return interpolate_focal
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import wave
-
+import torch
+import FocalCalculation
 import cv2
 import numpy as np
 from PIL import Image, ImageDraw
@@ -7,6 +8,13 @@ from PIL import Image, ImageDraw
 from ultralytics import YOLO
 
 UNIVERSAL_COUNT = 0
+CAR_REF_WIDTH = 78.6 #INCHES
+AVG_CAR_WIDTH = 70 #INCHES
+
+# distance finder function 
+def distance_finder(focal_length, real_object_width, width_in_frmae):
+    distance = (real_object_width * focal_length) / width_in_frmae
+    return distance
 
 def crop_image(img):
     try:
@@ -22,10 +30,6 @@ def crop_image(img):
     
 def predict_lights(img):
     try:
-        
-        
-        
-                
         # Make predictions on the input image
         results = model(img)
         
@@ -39,35 +43,78 @@ def predict_lights(img):
         prediction_index = 0
         prediction_boxes = zip(results[prediction_index].boxes.xyxy, results[prediction_index].boxes)
 
+        center_car = []
+
         for box in prediction_boxes:
-            if int(box[1].cls) != 9:  # Assuming class 9 corresponds to traffic light
-                continue
-            x_min, y_min, x_max, y_max = map(int, box[0])
+            if (int(box[1].cls) == 2) or (int(box[1].cls) == 7):
+                x_min, y_min, x_max, y_max = map(int, box[0])
 
-            # Crop the region
-            cropped_img = image_pil.crop((x_min, y_min, x_max, y_max))
+                height = y_max - y_min
+                width = x_max - x_min
+                center = (x_min + x_max) / 2
 
-            # Count the number of pixels for each color using HSV
-            red_pixels, yellow_pixels, green_pixels = count_hsv_pixels(np.array(cropped_img))
-            
-            
-            # Compare the counts to determine the traffic light color
-            if red_pixels > green_pixels and red_pixels > yellow_pixels:
-                predictions.append("red")
-                print("Traffic light color: Red", red_pixels, green_pixels, yellow_pixels)
-            elif green_pixels > red_pixels and green_pixels > yellow_pixels:
-                predictions.append("green")
-                print("Traffic light color: Green", red_pixels, green_pixels, yellow_pixels)
-            elif yellow_pixels > red_pixels and yellow_pixels > green_pixels:
-                predictions.append("yellow")
-                print("Traffic light color: Yellow", red_pixels, green_pixels, yellow_pixels)
-            else:
-                print("Unable to determine traffic light color")
+                distance = distance_finder(interpolate_focal(width), AVG_CAR_WIDTH, width)
 
-            # Display the cropped image
-            draw.rectangle([(x_min, y_min), (x_max, y_max)], outline="red", width=2)
-            
-        return predictions
+                if center > 520 and center < 920:
+                    center_car.append([center, distance, width, height])
+                    draw.rectangle([(x_min, y_min), (x_max, y_max)], outline="red", width=2)
+                    #add background rectangle behind text 
+                    draw.rectangle([(x_min, y_min - 20), (x_max, y_min)], fill=(0, 0, 0))
+                    draw.text((x_min, y_min - 20), model.names[int(box[1].cls)], fill=(255, 255, 255))
+                    #add distance
+                    draw.text((x_min, y_min - 10), f'{round(distance/12, 2)} feet', fill=(255, 255, 255))
+                    
+
+            elif int(box[1].cls) == 9:  # Assuming class 9 corresponds to traffic light
+                x_min, y_min, x_max, y_max = map(int, box[0])
+
+                # Crop the region
+                cropped_img = image_pil.crop((x_min, y_min, x_max, y_max))
+
+                # Count the number of pixels for each color using HSV
+                red_pixels, yellow_pixels, green_pixels = count_hsv_pixels(np.array(cropped_img))
+                
+                
+                # Compare the counts to determine the traffic light color
+                if red_pixels > green_pixels and red_pixels > yellow_pixels:
+                    predictions.append("red")
+                    print("Traffic light color: Red", red_pixels, green_pixels, yellow_pixels)
+                elif green_pixels > red_pixels and green_pixels > yellow_pixels:
+                    predictions.append("green")
+                    print("Traffic light color: Green", red_pixels, green_pixels, yellow_pixels)
+                elif yellow_pixels > red_pixels and yellow_pixels > green_pixels:
+                    predictions.append("yellow")
+                    print("Traffic light color: Yellow", red_pixels, green_pixels, yellow_pixels)
+                else:
+                    print("Unable to determine traffic light color")
+
+                draw.rectangle([(x_min, y_min), (x_max, y_max)], outline="red", width=2)
+                draw.text((x_min, y_min - 10), model.names[int(box[1].cls)], fill=(255, 255, 255))
+               
+
+        if len(center_car) > 0:
+            center_car = sorted(center_car, key=lambda x: abs(x[0] - 720))
+            #if two cars are within 150 pixels of each other, find the area of the bounding box
+            if len(center_car) > 1 and abs(center_car[0][0] - center_car[1][0]) < 150:
+                area1 = center_car[0][2] * center_car[0][3] * (150 - abs(720 - center_car[0][0]))
+                area2 = center_car[1][2] * center_car[1][3] * (150 - abs(720 - center_car[1][0]))
+
+                #show area on screen
+                # cv.putText(frame, f'Area1: {area1}', (center_car[0][0], 540), FONTS, 0.48, GREEN, 2)
+                # cv.putText(frame, f'Area2: {area2}', (center_car[1][0], 540), FONTS, 0.48, GREEN, 2)
+                # print("1",center_car)
+
+                if area1 > area2:
+                    # print("Area 1 Larger")
+                    center_car.pop(1)
+                else:
+                    # print("Area 2 Larger")
+                    center_car.pop(0)
+            pre_collision_dist = center_car[0][1]
+            print("2", center_car)
+            print(f"Distance: {pre_collision_dist} inches")
+
+        return np.array(image_pil)
     except Exception as e:
         print("An error occurred during prediction:", e)
         return []
@@ -107,34 +154,39 @@ def process_frame(frame):
     global UNIVERSAL_COUNT
     try:
         # Call the crop_image function to crop the frame
-        cropped_frame = crop_image(frame)
+        # cropped_frame = crop_image(frame)
         
         # Call the predict_lights function to predict traffic light colors
-        predictions = predict_lights(cropped_frame)
+        new_frame = predict_lights(frame)
         
         
         
-        return True
+        return new_frame
     except Exception as e:
         print("An error occurred during frame processing:", e)
         return False
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f'Using device: {device}')
 
+model = YOLO('yolov8n.onnx')
 
-model = YOLO('yolov8n.pt')
+interpolate_focal = FocalCalculation.CalculateFocalLengths()
 
 # Initialize YOLO model
 
 
 # Open webcam
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
+
+# print resolution of the camera
+print('Width :',cap.get(3))
+print('Height :',cap.get(4))
 
 # Check if the webcam is opened correctly
 if not cap.isOpened():
     print("Error: Unable to open webcam.")
 else:
-
-
 
     while True:
         # Capture frame-by-frame
@@ -147,11 +199,11 @@ else:
             
             # Process the frame
             processed_frame = process_frame(frame)
-            processed_frame = frame
+            processed_frame_bgr = cv2.cvtColor(processed_frame, cv2.COLOR_RGB2BGR)
 
             
             # Display the resulting frame
-            cv2.imshow('frame', processed_frame)
+            cv2.imshow('frame', processed_frame_bgr)
             
             # Stop capturing when 'q' is pressed
             if cv2.waitKey(1) & 0xFF == ord('q'):
