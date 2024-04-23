b8884e79b6fb48272575b36daf5d0a61d67f38a5
James Nolan <jamesn8391@gmail.com>
2024-04-23 14:16:04 -0500

    added route to open file explorer

--- server/server.py
+++ server/server.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
+import os
+import webbrowser
 
 app = Flask(__name__)
 
@@ -16,6 +18,14 @@ def greet():
         return jsonify({'message': f'Hello, {data["name"]}!'})
     else:
         return jsonify({'error': 'Missing "name" parameter'}), 400
+
+@app.route('/open_file_explorer', methods=['GET'])
+def open():
+    downloads_folder = os.path.expanduser("~/Downloads")
+    
+    webbrowser.open_new_tab(downloads_folder)
+    return jsonify({"message": "Opened downloads folder"})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3001) 
