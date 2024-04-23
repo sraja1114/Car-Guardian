4e7d9a55729b84f3a783bf62b0ebfd680ce52430
Emil Agbigay <emilagbigay@tamu.edu>
2024-04-23 14:52:27 -0500

    fix styling merge

--- gui/src/Components/ButtonList/ButtonList.js
+++ gui/src/Components/ButtonList/ButtonList.js
@@ -6,10 +6,25 @@ import "./ButtonList.css";
 
 export default function ButtonList() {
   return (
+<<<<<<< HEAD
+    <>
+    <div className="button-list-container"> {/* Use the CSS class */}
+    <div>
+      <img src={myImage} alt="My Image" className="my-image" />
+    </div>
+      <Stack spacing={3} direction="column">
+        <Button variant="outlined" class="MuiButton-label">Sensitivity</Button>
+        <Button variant="outlined" class="MuiButton-label">Dashcam Videos</Button>
+        <Button variant="outlined" class="MuiButton-label">Record</Button>
+      </Stack>
+    </div>
+    </>
+=======
     <Stack spacing={3} direction="column">
-      <Button variant="outlined">Outlined</Button>
-      <Button variant="outlined">Outlined</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Button variant="outlined">Open Files</Button>
+      <Button variant="outlined">Save last 30 seconds</Button>
+      <Button variant="outlined">Sensitivity</Button>
     </Stack>
+>>>>>>> 4edac9c84d87e86c2c9d8855b8a2b433921c75f4
   );
 }
