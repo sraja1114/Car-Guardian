a8d36060de4e642f4e432b32262900baf53b445c
Emil Agbigay <emilagbigay@tamu.edu>
2024-04-23 15:04:04 -0500

    merge, and logo added

--- gui/src/Components/ButtonList/ButtonList.js
+++ gui/src/Components/ButtonList/ButtonList.js
@@ -1,14 +1,22 @@
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import * as React from 'react';
+import myImage from './logo.png';
 import "./ButtonList.css";
 
 export default function ButtonList() {
   return (
-    <Stack spacing={3} direction="column">
-      <Button variant="outlined" class="MuiButton-label">Open Files</Button>
-      <Button variant="outlined" class="MuiButton-label">Save last 30 seconds</Button>
-      <Button variant="outlined" class="MuiButton-label">Sensitivity</Button>
-    </Stack>
+    <>
+    <div className="button-list-container"> {/* Use the CSS class */}
+    <div>
+      <img src={myImage} alt="My Image" className="my-image" />
+    </div>
+      <Stack spacing={3} direction="column">
+        <Button variant="outlined" class="MuiButton-label">Sensitivity</Button>
+        <Button variant="outlined" class="MuiButton-label">Dashcam Videos</Button>
+        <Button variant="outlined" class="MuiButton-label">Record</Button>
+      </Stack>
+    </div>
+    </>
   );
 }
